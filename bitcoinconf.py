73c25a41c1610c6e96b259703118f6febb0ad602
Jacob Welsh <jwelsh@welshcomputing.com>
2014-08-24 19:46:06 -0500

    More tabs to spaces

--- bitcoinconf.py
+++ bitcoinconf.py
@@ -6,35 +6,35 @@
 import platform, os
 
 if platform.system() == 'Windows':
-	datadir = os.path.join(os.environ['APPDATA'], 'Bitcoin')
+    datadir = os.path.join(os.environ['APPDATA'], 'Bitcoin')
 else:
-	_home = os.environ['HOME']
-	if platform.system() == 'Darwin':
-		datadir = os.path.join(_home, 'Library/Application Support/Bitcoin')
-	else:
-		datadir = os.path.join(_home, '.bitcoin')
+    _home = os.environ['HOME']
+    if platform.system() == 'Darwin':
+        datadir = os.path.join(_home, 'Library/Application Support/Bitcoin')
+    else:
+        datadir = os.path.join(_home, '.bitcoin')
 
 conf='bitcoin.conf'
 
 COIN=100000000
 
 def read():
-	"""Read a bitcoin.conf file, returning its key/value pairs as a dict.
+    """Read a bitcoin.conf file, returning its key/value pairs as a dict.
 
-	To change datadir/conf, set datadir or conf in the module namespace.
-	"""
-	f = open(os.path.join(datadir, conf), 'r')
-	lines = f.readlines()
-	f.close()
-	c = {}
-	for line in lines:
-		if len(line) == 0:
-			continue
-		line = line.split('#', 1)[0]
-		if len(line) == 0:
-			continue
-		parts = line.split('=', 1)
-		if len(parts) != 2:
-			continue
-		c[parts[0].strip()] = parts[1].strip()
-	return c
+    To change datadir/conf, set datadir or conf in the module namespace.
+    """
+    f = open(os.path.join(datadir, conf), 'r')
+    lines = f.readlines()
+    f.close()
+    c = {}
+    for line in lines:
+        if len(line) == 0:
+            continue
+        line = line.split('#', 1)[0]
+        if len(line) == 0:
+            continue
+        parts = line.split('=', 1)
+        if len(parts) != 2:
+            continue
+        c[parts[0].strip()] = parts[1].strip()
+    return c
--- formatting.py
+++ formatting.py
@@ -5,51 +5,51 @@
 
 class ByteCountFormatter:
 
-	"""Human-readable formatting of byte counts. Stores a formatting mode
-	(bit vs. byte and unit system) for consistent display in different
-	parts of a program.
+    """Human-readable formatting of byte counts. Stores a formatting mode
+    (bit vs. byte and unit system) for consistent display in different
+    parts of a program.
 
-	By default, the formatter uses SI and bytes, so 1000 => "1 KB". All
-	combinations of (byte, bit) x (SI, binary) are supported, though you
-	probably shouldn't use bits with the binary prefixes.
-	"""
+    By default, the formatter uses SI and bytes, so 1000 => "1 KB". All
+    combinations of (byte, bit) x (SI, binary) are supported, though you
+    probably shouldn't use bits with the binary prefixes.
+    """
 
-	SI_prefixes = ('k','M','G','T','P','E','Z','Y')
-	binary_prefixes = ('Ki','Mi','Gi','Ti','Pi','Ei','Zi','Yi')
+    SI_prefixes = ('k','M','G','T','P','E','Z','Y')
+    binary_prefixes = ('Ki','Mi','Gi','Ti','Pi','Ei','Zi','Yi')
 
-	def __init__(self):
-		self._use_bits = False
-		self._use_si = True
+    def __init__(self):
+        self._use_bits = False
+        self._use_si = True
 
-	def setUnitBytes(self):
-		self._use_bits = False
-	def setUnitBits(self):
-		self._use_bits = True
-	def setPrefixSI(self):
-		self._use_si = True
-	def setPrefixBinary(self):
-		self._use_si = False
+    def setUnitBytes(self):
+        self._use_bits = False
+    def setUnitBits(self):
+        self._use_bits = True
+    def setPrefixSI(self):
+        self._use_si = True
+    def setPrefixBinary(self):
+        self._use_si = False
 
-	def format(self, count):
-		"""Formats a byte count using the configured settings."""
-		if self._use_bits:
-			count *= 8
-			unit = 'b'
-		else:
-			unit = 'B'
+    def format(self, count):
+        """Formats a byte count using the configured settings."""
+        if self._use_bits:
+            count *= 8
+            unit = 'b'
+        else:
+            unit = 'B'
 
-		if self._use_si:
-			factor = 1000.
-			prefixes = self.SI_prefixes
-		else:
-			factor = 1024.
-			prefixes = self.binary_prefixes
+        if self._use_si:
+            factor = 1000.
+            prefixes = self.SI_prefixes
+        else:
+            factor = 1024.
+            prefixes = self.binary_prefixes
 
-		if abs(count) < factor:
-			return u'%d %c' % (count, unit)
-		size = float(count)
-		prefixIndex = 0
-		while abs(size) >= factor and prefixIndex < len(prefixes):
-			size /= factor
-			prefixIndex += 1
-		return u'%.2f %s%c' % (size, prefixes[prefixIndex-1], unit)
+        if abs(count) < factor:
+            return u'%d %c' % (count, unit)
+        size = float(count)
+        prefixIndex = 0
+        while abs(size) >= factor and prefixIndex < len(prefixes):
+            size /= factor
+            prefixIndex += 1
+        return u'%.2f %s%c' % (size, prefixes[prefixIndex-1], unit)

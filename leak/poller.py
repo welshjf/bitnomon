5c138f997c7a890f416a4f7fbd54b18db5a1a136
Jacob Welsh <jwelsh@welshcomputing.com>
2014-08-28 17:13:06 -0500

    Memory testing poller tweaks

--- leak/poller.py
+++ leak/poller.py
@@ -1,12 +1,13 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 import sys, signal
-from PySide import QtCore, QtNetwork
-#from PyQt4 import QtCore, QtNetwork
-#QtCore.Signal = QtCore.pyqtSignal
-#QtCore.Slot = QtCore.pyqtSlot
+#from PySide import QtCore, QtNetwork
+from PyQt4 import QtCore, QtNetwork
+QtCore.Signal = QtCore.pyqtSignal
+QtCore.Slot = QtCore.pyqtSlot
 
 class Poller(QtCore.QObject):
+
     url = QtCore.QUrl("http://localhost:5000/")
 
     def __init__(self, parent=None):
@@ -15,22 +16,23 @@ class Poller(QtCore.QObject):
 
     def start(self):
         request = QtNetwork.QNetworkRequest(self.url)
-        self.reply = self.manager.get(request)
+        self.reply = self.manager.post(request, '')
+        self.reply.setParent(None)
         self.reply.finished.connect(self.readReply)
         self.reply.error.connect(self.error)
 
     def readReply(self):
         text = self.reply.readAll()
-        #self.reply.close() # doesn't help
-        #self.reply.deleteLater() # doesn't help
-        self.reply.setParent(None)
-        #QtCore.QTimer.singleShot(10, self.start)
-        QtCore.QTimer.singleShot(0, self.start)
+        self.reply = None
+        QtCore.QTimer.singleShot(10, self.start)
 
     @QtCore.Slot(QtNetwork.QNetworkReply.NetworkError)
     def error(self, err):
-        sys.stderr.write('Error: ' + str(err) + '\n')
-        QtCore.QCoreApplication.instance().quit()
+        sys.stderr.write('Error: {}\n'.format(self.sender().errorString()))
+        self.reply.finished.disconnect()
+        self.reply = None
+        #QtCore.QCoreApplication.instance().quit()
+        QtCore.QTimer.singleShot(10, self.start)
 
 signal.signal(signal.SIGINT, signal.SIG_DFL)
 app = QtCore.QCoreApplication(sys.argv)

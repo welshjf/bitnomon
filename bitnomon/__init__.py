a7c628745b9d2adce19f15e566e78e17938df4b3
Jacob Welsh <jwelsh@welshcomputing.com>
2015-01-18 02:16:28 -0500

    Support bundling pyqtgraph + other setup tweaks

--- bitnomon/__init__.py
+++ bitnomon/__init__.py
@@ -1,3 +1,10 @@
 """Monitoring GUI for a Bitcoin Core node"""
 
 __version__ = '0.1'
+
+# Whether to bundle dependencies (pyqtgraph) inside the package
+BUNDLE = True
+
+if BUNDLE:
+    import sys, os
+    sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'deps'))
--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 from setuptools.command.sdist import sdist as _sdist
 from distutils import log
 import subprocess
-from bitnomon import __version__
+from bitnomon import __version__, BUNDLE
 
 class sdist(_sdist):
 
@@ -24,10 +24,29 @@ class sdist(_sdist):
 with open('README.rst') as f:
     readme_text = f.read()
 
+packages = ['bitnomon']
+package_dir = {}
+requires=[
+    'appdirs (>=1.3.0)',
+    'numpy',
+    'PyQt4 (>=4.7.0)',
+    # The binding distributed with rrdtool itself is version 1.x; we want
+    # the newer one from PyPI which supports Python 3.
+    'rrdtool (>=0.1.0, <1.0.0)',
+]
+if BUNDLE:
+    pg_pkgs = find_packages('deps/pyqtgraph', exclude=['examples*'])
+    packages.extend('bitnomon.deps.' + pkg for pkg in pg_pkgs)
+    package_dir['bitnomon.deps.pyqtgraph'] = 'deps/pyqtgraph/pyqtgraph'
+else:
+    # 0.9.8 is too old; 0.9.9/10 shipped with a drawing bug that affects us
+    # (https://github.com/pyqtgraph/pyqtgraph/pull/136)
+    requires.append('pyqtgraph (>0.9.10)')
+
 setup(
     name='bitnomon',
     version=__version__,
-    description='Bitcoin Node Monitor',
+    description='Monitoring/visualization GUI for a Bitcoin node',
     long_description=readme_text,
     author='Jacob Welsh',
     author_email='jacob@welshcomputing.com',
@@ -35,27 +54,25 @@ setup(
     license='MIT',
     classifiers=[
         'Development Status :: 3 - Alpha',
+        'Environment :: MacOS X',
+        'Environment :: Win32 (MS Windows)',
         'Environment :: X11 Applications :: Qt',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: System Administrators',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Operating System :: POSIX',
         'Operating System :: MacOS :: MacOS X',
         'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Topic :: Scientific/Engineering :: Visualization',
         'Topic :: System :: Monitoring',
     ],
-    packages=['bitnomon'],
-    requires=[
-        # 0.9.8 is too old; 0.9.9/10 shipped with a drawing bug that affects us
-        # (https://github.com/pyqtgraph/pyqtgraph/pull/136)
-        'pyqtgraph (>0.9.10)',
-        # The binding distributed with rrdtool itself is version 1.x; we want
-        # the newer one from PyPI which supports Python 3.
-        'rrdtool (>=0.1.0, <1.0.0)',
-        'appdirs (>=1.3.0)',
-    ],
+    packages=packages,
+    package_dir=package_dir,
+    requires=requires,
     package_data={
         'bitnomon': [],
     },
